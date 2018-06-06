dd2ed399d9d1b51583ecb8701bbccca23b889c4d
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-06 15:44:11 -0400

    Create lists for jags data, params, and make calls to runModel

--- R/autoBBS.R
+++ R/autoBBS.R
@@ -1,12 +1,13 @@
 autoBBS <- function(speciesList = NULL,
                     modelName = NULL,
+                    inits = NULL,
                     adaptSteps = 500,
                     burnInSteps = 20000,
                     nChains = 3,
                     numSavedSteps = 2000,
                     thinSteps = 10,
                     nIter = ceiling( ( numSavedSteps * thinSteps ) / nChains ),
-                    parallel = TRUE)
+                    runParallelChains = FALSE)
 {
   processInput(speciesList, modelName)
 
@@ -27,6 +28,49 @@ autoBBS <- function(speciesList = NULL,
                                  data.cleaned$sptorun,
                                  data.cleaned$sptorun2,
                                  index, modelName)
+
+    data.jags <- list(ncounts = nrow(data.prep$spsp.f),
+                      nstrata=length(unique(data.prep$spsp.f$strat)),
+                      ymin = data.prep$ymin,
+                      ymax = data.prep$ymax,
+                      nonzeroweight = data.prep$pR.wts$p.r.ever,
+                      count = as.integer(data.prep$spsp.f$count),
+                      strat = as.integer(data.prep$spsp.f$strat),
+                      obser = as.integer(data.prep$spsp.f$obser),
+                      year = data.prep$spsp.f$year,
+                      firstyr = data.prep$spsp.f$firstyr,
+                      nobservers = data.prep$nobservers)
+    if (tolower(modelName) == "standard")
+    {
+      data.jags <- c(data.jags, list(fixedyear = midyear))
+    }
+
+    sp.params = c("sdbeta",
+                  "strata",
+                  "STRATA",
+                  "sdstrata",
+                  "sdobs",
+                  "obs",
+                  "n",
+                  "posdiff",
+                  "sdnoise",
+                  "eta",
+                  "overdisp",
+                  "nfzero",
+                  "gof",
+                  "fgof",
+                  "diffgof")
+
+    jagsjob <- runModel(data.jags,
+                            NULL,
+                            sp.params,
+                            mod = standard,
+                            nChains,
+                            adaptSteps,
+                            nIter,
+                            burnInSteps,
+                            thinSteps,
+                            runParallelChains)
     spNum <- spNum + 1
   }
 }
