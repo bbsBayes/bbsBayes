73231bc4b3434f40ac170447acb8ae35254e4a3e
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-06 14:13:48 -0400

    standard model code

---
+++ R/standard-model.R
@@ -0,0 +1,93 @@
+standard <- function()
+{
+  #### counts and overdispersion effects  ###### Link-Sauer model with nested observer-route effect
+  for( k in 1 : ncounts )
+  {
+    log(lambda[k]) <- beta[strat[k]] * (year[k] - fixedyear) + eta*firstyr[k] + obs[strat[k],obser[k]] + strata[strat[k]] + yeareffect[year[k],strat[k]]
+
+    noise[k] ~ dnorm(0.0, taunoise)
+    count[k] ~ dpois(lambda[k])
+    #----------------------------------#
+    fcount[k] ~ dpois(lambda[k])
+    err[k] <- pow(count[k]-lambda[k],2)/lambda[k]
+    ferr[k] <- pow(fcount[k]-lambda[k],2)/lambda[k]
+    fzero[k] <- equals(fcount[k],0)
+    loglik[k] <- logdensity.pois(count[k], lambda[k])
+    #----------------------------------#
+  }
+
+  nfzero <- sum(fzero[1:ncounts])
+  gof <- sum(err[1:ncounts])
+  fgof <- sum(ferr[1:ncounts])
+  diffgof <- gof-fgof
+  posdiff <- step(diffgof)
+
+  taunoise ~ dgamma(0.001,0.001)
+  nu ~ dgamma(2, 0.1) #prior suggested by Bill Link in email dated March 27, 2018
+  sdnoise <- 1 / pow(taunoise, 0.5)
+  sdobs <- 1 / pow(tauobs, 0.5)
+  tauobs ~ dgamma(0.001,0.001)
+  eta ~ dnorm( 0.0,1.0E-6)
+  STRATA ~ dnorm( 0.0,0.01)
+  taustrata ~ dgamma(0.001,0.0001) #<- 1/pow(sdbeta,2)#
+  sdstrata <- 1/pow(taubeta,0.5)#~ dunif(0.001,10)
+
+  #---------------------------------------------------------#
+
+  #----------------------------------#
+  #### stratum effects  ######
+  for( s in 1 : nstrata )
+  {
+    #### observer effects  ######
+
+    for( i in 1 : nobservers[s] )
+    {
+      obs[s,i] ~ dnorm( 0.0,tauobs)
+    }
+
+    #### end observer effects  ######
+
+    beta[s] ~ dnorm(BETA,taubeta)
+    strata[s] ~ dnorm(STRATA,taustrata)
+    sdyear[s] <- 1/pow(tauyear[s],0.5)
+    tauyear[s] ~ dgamma(0.001,0.001) #
+
+    #### stratum specific year effects  ######
+    for( y in ymin : ymax )
+    {
+      yeareffect[y,s] ~ dnorm( 0.0, tauyear[s])
+    }
+
+    expstrata[s] <- exp(strata[s])
+    overdisp[s] <- 1 + 1/(expstrata[s]*taunoise)
+    #-------------------------------------------------#
+
+  }# end s strata loop
+
+
+
+  #### hierarchical trend effects  ######
+
+
+  BETA ~ dnorm(0,0.01)
+
+  taubeta ~ dgamma(1,0.0001) #
+  ### informative taubeta, parameterized so that:
+  ### mean of taubeta = ~10000
+  ### the mean of sdbeta =~ 0.02
+  ### there is <5% prob of sdbeta > 0.04 (i.e., there is a small, but non-zero probability that the SD of the stratum beta
+  ### terms is large enough to to encompass trends that vary from -8%/year to 8%/year with a continental trend = 0)
+  ### this informative prior should only be useful when there are few strata
+
+  sdbeta <- 1/pow(taubeta,0.5)#
+
+  for( i in 1 : nstrata )
+  {
+    for( t in ymin : ymax )
+    {
+      n[i,t] <- nonzeroweight[i]*exp(strata[i] + beta[i]*(t-fixedyear) + yeareffect[t,i] + 0.5*sdnoise*sdnoise+ 0.5*sdobs*sdobs)
+    }
+  }
+
+  #-------------------------------------------------#
+}
