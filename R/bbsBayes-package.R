0d7fb3cc4c55886d06020632c1aeb7fd2c02a954
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-07 13:35:15 -0400

    Load individual RData files.

--- R/bbsBayes-package.R
+++ R/bbsBayes-package.R
@@ -1,5 +1,10 @@
 .onLoad <- function(libname, pkgname)
 {
   # Eventually this might just be a web scraper from USGS site
-  data("bbs", package = pkgname, envir = parent.env(environment()))
+  data("birds", package = pkgname, envir = parent.env(environment()))
+  data("datacount.sp", package = pkgname, envir = parent.env(environment()))
+  data("route", package = pkgname, envir = parent.env(environment()))
+  data("sp", package = pkgname, envir = parent.env(environment()))
+  data("st.areas", package = pkgname, envir = parent.env(environment()))
+  data("midyear", package = pkgname, envir = parent.env(environment()))
 }
