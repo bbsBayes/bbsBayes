5ce2eaa226136896cc583ebd9b721dd51765729c
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-07 10:32:06 -0400

    Check for output directory and print warning if none was supplied

--- R/process-input.R
+++ R/process-input.R
@@ -1,11 +1,13 @@
 processInput <- function(speciesList,
-                         modelName)
+                         modelName, outputDir)
 {
+  # Check for at least 1 species to test.
   if (is.null(speciesList))
   {
     stop("no species were provided to autoBBS().\n")
   }
 
+  # Check for at least 1 model to test
   if (is.null(modelName))
   {
     stop("no model was specified to autoBBS().\n")
@@ -16,5 +18,13 @@ processInput <- function(speciesList,
     {
       stop(paste(modelName, "is not a valid model.\n"))
     }
+  }
+
+  # Check for output directory
+  if (is.null(outputDir))
+  {
+    warning(paste("no output directory was specified.",
+                  "autoBBS() will default to sending output to",
+                  getwd()))
   }
 }
