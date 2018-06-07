00a387b0ef7f41fcfb1404bb5c231b3d6484014f
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-07 13:49:23 -0400

    roxygen docs for autoBBS

--- R/autoBBS.R
+++ R/autoBBS.R
@@ -1,3 +1,36 @@
+#' Automatically run a BBS analysis for a given species
+#'
+#' \code{autoBBS} generates MCMC chains for a list of species using BBS
+#'   data.
+#'
+#' @param species Character strings or vector of character strings of what
+#'   species are wanting to be analysed
+#' @param model Character strings or vector of character strings of what
+#'   species are wanting to be analysed.
+#' @param outputDir Path to directory where output should be directed to.
+#'   Defaults to current directory.
+#' @param inits Optional list of initialization values for JAGS model.
+#'   If none are specified, the JAGS model will generate its own
+#'   initial values.
+#' @param adaptSteps Optional integer specifying the number of steps to
+#'   adapt the JAGS model. It is recommended to do a minimum of 100,
+#'   \code{autoBBS} will default to 500.
+#' @param burnInSteps Optional integer specifying the number of iterations
+#'   to burn in the model. Defaults to 20000 per chain.
+#' @param nChains Optional number of chains to run. Defaults to 3.
+#' @param numSavedSteps Optional number of steps to save per chain.
+#'   Defaults to 2000.
+#' @param thinSteps Optional number of steps to thin or discard.
+#' @param nIter Optional number of iterations.
+#' @param runParallelChains Optional boolean value to specify whether
+#'   the chains are run in parallel. By default, the chains are not
+#'   run in parallel. If TRUE, the number of cores used will be the
+#'   minimum of number of chains and number of available cores.
+#'   JAGS console output is supressed with run in parallel.
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
 autoBBS <- function(species = NULL,
                     model = NULL,
                     outputDir = NULL,
