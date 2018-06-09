c76f27b71f9c67f9831f3cf42095596f45ca84af
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-09 15:56:24 -0400

    Model dictionary

--- R/bbsBayes-package.R
+++ R/bbsBayes-package.R
@@ -1,10 +1,7 @@
 .onLoad <- function(libname, pkgname)
 {
-  # Eventually this might just be a web scraper from USGS site
-  data("birds", package = pkgname, envir = parent.env(environment()))
-  data("datacount.sp", package = pkgname, envir = parent.env(environment()))
-  data("route", package = pkgname, envir = parent.env(environment()))
-  data("sp", package = pkgname, envir = parent.env(environment()))
-  data("st.areas", package = pkgname, envir = parent.env(environment()))
-  data("midyear", package = pkgname, envir = parent.env(environment()))
+  assign("models",
+         list(standard = "standard.jags",
+                 fd = "first-difference.jags"),
+         parent.env(environment()))
 }
