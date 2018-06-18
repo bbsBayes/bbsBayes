6e90e315ad1deb914843eeceab4578a450b159ad
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-18 16:40:47 -0400

    Lookup list for default stratifications

--- R/bbsBayes-package.R
+++ R/bbsBayes-package.R
@@ -4,4 +4,11 @@
          list(standard = "standard.jags",
                  fd = "first-difference.jags"),
          parent.env(environment()))
+
+  assign("strata",
+         list(bbs = "bbs.csv",
+              state = "state-prov.csv",
+              bcr = "bcr.csv",
+              latlong = "degree-blocks.csv"),
+         parent.env(environment()))
 }
