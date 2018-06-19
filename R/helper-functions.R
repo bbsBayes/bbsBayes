d3a93cae5ee8d7c42e9167c438067eef0c257400
Brandon Edwards <brandon.edwards144@gmail.com>
2018-06-19 11:52:02 -0400

    French and Spanish species AOU lookup

--- R/helper-functions.R
+++ R/helper-functions.R
@@ -1,4 +1,21 @@
 get_species_aou <- function(species, sp_eng)
 {
-  return(species[species$english == sp_eng, "sp.bbs"])
+  aou <- NULL
+  if (length(aou <- species[species$english == sp_eng, "sp.bbs"]) == 1)
+  {
+    return(aou)
+  }
+  else if (length(aou <- species[species$french == sp_eng, "sp.bbs"]) == 1)
+  {
+    return(aou)
+  }
+  else if (length(aou <- species[species$spanish == sp_eng, "sp.bbs"]) == 1)
+  {
+    return(aou)
+  }
+  else
+  {
+    return(-1)
+  }
+
 }
