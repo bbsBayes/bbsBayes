5bb5963fbf42f1f5b20589606f0229f3b29d09b4
Brandon Edwards <brandon.edwards144@gmail.com>
2018-07-24 19:15:36 -0400

    Obtain strata areas as data frame. Resolves #21

---
+++ R/get-strata-area.R
@@ -0,0 +1,51 @@
+#' Get the area of each strata
+#'
+#' \code{get_strata_area} allows you to obtain the area of each strata
+#'   for a given stratification type.
+#'
+#' @param filename strata_type
+#'
+#' @return Data frame with the following variables:
+#'   \item{region}{Name of the stratum/region}
+#'   \item{area_sq_km}{Area of the stratum/region in square kilometres}
+#'
+#' @importFrom utils read.csv
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' # Obtain the strata area for each of the 5 stratification types
+#'
+#' # USGS BBS
+#' st_area <- get_strata_area(strata_type = "bbs_usgs")
+#'
+#' # CWS BBS
+#' st_area <- get_strata_area(strata_type = "bbs_cws")
+#'
+#' # BCR
+#' st_area <- get_strata_area(strata_type = "bcr")
+#'
+#' # State/Province/Territory
+#' st_area <- get_strata_area(strata_type = "state")
+#'
+#' # Degree block
+#' st_area <- get_strata_area(strata_type = "latlong")
+#' }
+#'
+
+get_strata_area <- function(strata_type = NULL)
+{
+  if (strata_type %in% c("bbs_usgs",
+                         "bbs_cws",
+                         "bcr",
+                         "state",
+                         "latlong"))
+  {
+    return(read.csv(system.file("area-weight", strata[[strata_type]], package = "bbsBayes")))
+  }else
+  {
+    stop("Not a valid stratification type.")
+  }
+}
