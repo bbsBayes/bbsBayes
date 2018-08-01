bf444a87a0d61d720b11abc5be3e7decae96802b
Brandon Edwards <brandon.edwards144@gmail.com>
2018-08-01 16:11:13 -0400

    Add map files and skeleton mapping function. Resolves #41

--- R/bbsBayes-package.R
+++ R/bbsBayes-package.R
@@ -1,5 +1,13 @@
 .onLoad <- function(libname, pkgname)
 {
+  assign("maps",
+         list(bbs_usgs = "BBS_USGS_strata",
+              bbs_cws = "BBS_CWS_strata",
+              state = "BBS_ProvState_strata",
+              bcr = "BBS_BCR_strata",
+              latlong = "BBS_LatLong_strata"),
+         parent.env(environment()))
+
   assign("models",
          list(slope = "slope.jags",
               firstdiff = "first-difference.jags",
