9bc74ec0287ef297f64fdd47a859af7135e0fd44
Brandon Edwards <brandon.edwards144@gmail.com>
2019-07-17 20:17:44 -0400

    Get final values when JAGS is run with parallel chains. Close #124

--- R/get-final-values.R
+++ R/get-final-values.R
@@ -17,6 +17,14 @@ get_final_values <- function(model = NULL)
   if (is.null(model))
   {
     return(NULL)
+  }else if (model$parallel){
+    to_return <- vector("list", length(model$model))
+    for (i in 1:length(model$model))
+    {
+      to_return[[i]] <- eval(parse(text = (paste0("as.list(model$model$cluster",
+                                                        i,
+                                                        "$state()[[1]])"))))
+    }
   }else{
     return(as.list(model$model$state()))
   }
