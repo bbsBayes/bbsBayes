88afc728bd3b78063e5b9ac41e16c47c703ace18
Brandon Edwards <brandon.edwards144@gmail.com>
2021-07-16 10:32:53 -0400

    Load stop level data, and update citation

--- R/load-bbs-data.R
+++ R/load-bbs-data.R
@@ -5,6 +5,14 @@
 #' Note that this function is not necessary to run a Bayesian analysis of BBS data;
 #' calling \code{stratify} will return stratifed BBS data in a list of data frames.
 #'
+#' @param level A string, either "state" or "stop", specifying which counts to
+#' load. Defaults to "state", which provides counts beginning in 1966,
+#' aggregated in five bins, each of which contains cumulative counts from 10 of
+#' the 50 stops along a route. Specifying "stop" provides stop-level data
+#' beginning in 1997, which includes counts for each stop along routes
+#' individually. Note that stop-level data is not currently supported by
+#' the modeling utilities in bbsBayes.
+#'
 #' @return Large list (3 elements) consisting of:
 #' \item{bird}{Data frame of sample bird point count data per route, per year}
 #' \item{route}{Data frame of sample yearly route data}
@@ -13,19 +21,30 @@
 #' @export
 #'
 
-load_bbs_data <- function()
+load_bbs_data <- function(level = "state")
 {
   bbs_data <- NULL
   rm(bbs_data)
 
   bbs_dir <- app_dir(appname = "bbsBayes")
 
-  if(isFALSE(file.exists(paste0(bbs_dir$data(), "/bbs_raw_data.RData"))))
+  if (level == "state")
   {
-    stop("No BBS data downloaded. Please use fetch_bbs_data() first.")
-  }
+    if(isFALSE(file.exists(paste0(bbs_dir$data(), "/bbs_raw_data.RData"))))
+    {
+      stop("No BBS data downloaded. Please use fetch_bbs_data() first.")
+    }
 
-  load(file = paste0(bbs_dir$data(), "/bbs_raw_data.RData"))
+    load(file = paste0(bbs_dir$data(), "/bbs_raw_data.RData"))
+  }else if (level == "stop")
+  {
+    if(isFALSE(file.exists(paste0(bbs_dir$data(), "/bbs_stop_data.RData"))))
+    {
+      stop("No BBS stop data downloaded. Please use fetch_bbs_data(level = \"stop\") first.")
+    }
+
+    load(file = paste0(bbs_dir$data(), "/bbs_stop_data.RData"))
+  }
 
   return(bbs_data)
 }
