0e5e385194d76d1b97de568be6982eeff3b1b767
steffilazerte <steffi@steffi.ca>
2022-11-02 15:56:03 -0500

    Add convergence()

--- R/convergence.R
+++ R/convergence.R
@@ -8,25 +8,35 @@
 #'
 #' @examples
 #'
-#' load(system.file("model_outputs", "pacific_wren_slope_BBS_fit.RData", package = "bbsBayes"))
+#' # Toy example with Pacific Wren sample data
+#' # First, stratify the sample data
+#' s <- stratify(by = "bbs_cws", sample_data = TRUE)
 #'
-convergence <- function(model_fit) {
+#' # Prepare the stratified data for use in modelling
+#' d <- prepare_data(s, species = "Pacific Wren",
+#'                   min_year = 2009,
+#'                   max_year = 2018)
+#'
+#' # Now run the model (fast but not good, just for illustration)
+#' m <- run_model(d, model = "first_diff",
+#'                iter_sampling = 20, iter_warmup = 20, chains = 2)
+#'
+#' # Calculate convergence metrics on each variable
+#' conv <- convergence(m)
+#'
 
-  draws <- model_fit$draws(variables = "strata")
+convergence <- function(model_output) {
 
-  vars <- posterior::variables(draws)
+  model_fit <- model_output$model_fit
 
-  draws <- model_fit$draws(variables = stringr::str_subset(vars, "strata_raw"))
-
-  model_fit$summary()
-
-  conv <- data.frame(variable = c("yeareffect", "strata")) %>%
-    dplyr::mutate(v = purrr::map(variable, ~stringr::str_subset(vars, paste0(.x, "_raw"))),
-                  d = purrr::map(v, ~model_fit$draws(variables = .x))) %>%
-    dplyr::mutate(ess_bulk = purrr::map(d, posterior::ess_bulk),
-                  ess_tail = purrr::map(d, posterior::ess_tail),
-                  rhat = purrr::map(d, posterior::rhat))
-
-
+  # Calculate convergence metrics on *each* variable
+  dplyr::tibble(variable = posterior::variables(draws)) %>%
+    dplyr::mutate(d = purrr::map(variable, ~model_fit$draws(variables = .x))) %>%
+    dplyr::mutate(
+      ess_bulk = purrr::map_dbl(d, posterior::ess_bulk),
+      ess_tail = purrr::map_dbl(d, posterior::ess_tail),
+      rhat = purrr::map_dbl(d, posterior::rhat),
+      variable_type = stringr::str_remove(variable, "\\[[0-9]+\\]$")) %>%
+    dplyr::select("variable_type", "variable", "ess_bulk", "ess_tail", "rhat")
 }
 
