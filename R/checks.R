b921d74a3fe06f6cb8784b1bcbcc86050bc1ba17
steffilazerte <steffi@steffi.ca>
2022-11-02 15:40:28 -0500

    add more checks

--- R/checks.R
+++ R/checks.R
@@ -1,3 +1,34 @@
+check_data <- function(data) {
+
+  type <- deparse(substitute(data))
+  n <- c("meta_data", "meta_strata")
+
+  if(type == "strata_data") {
+    n <- c(n, "routes_strata", "birds_strata")
+    from <- "stratify()"
+  } else if(type == "prepped_data") {
+    n <- c(n, "model_data", "raw_data")
+    from <- "prepare_data()"
+  } else if(type == "spatial_data") {
+    # Don't use meta_data or meta_strata, n is an actual object returned
+    n <- c("n", "n_edges", "node1", "node2", "adj_matrix", "strata_name")
+    from <- "prepare_spatial()"
+  } else if(type == "model_output") {
+    n <- c(n, "model_fit", "non_zero_weight", "raw_data")
+    from <- "run_model()"
+  } else if(type == "indices") {
+    n <- c(n, "data_summary", "samples", "raw_data")
+    from <- "generate_indices()"
+  } else if (type == "trends") {
+
+  }
+
+  if(!is.list(data) || !all(names(data) %in% n)) {
+    stop("`", type, "` must a list created by `", from, "` ",
+         "containing\n`", paste0(n, collapse = "`, `"), "`", call. = FALSE)
+  }
+}
+
 
 #' Check model name
 #'
@@ -55,44 +86,189 @@ check_basis <- function(basis) {
 
 #' Check stratification
 #'
-#' @param strat Stratification type
+#' @param strata Stratification type
 #' @param name Name of argument to return if missing.
+#' @param custom sf data frame of a map for custom strata
 #'
 #' @noRd
-check_stratification <- function(strata, name = "stratification (`by`)") {
-  if(is.null(strata)) stop("No ", name, " specified", call. = FALSE)
+check_strata <- function(strata, custom = NULL, simple = FALSE,
+                         quiet = TRUE) {
+  if(is.null(strata)) stop("No stratification (`by`) specified", call. = FALSE)
   strata <- tolower(strata)
-  s <- c("state", "bcr", "latlong", "bbs_cws", "bbs_usgs")
-  if(!strata %in% s) {
-    stop("Invalid stratification specified, choose one of '",
-         paste0(s, collapse = "', '"), "'", call. = FALSE)
+
+  type <- "standard"
+
+  # Simple checks
+  if(simple) {
+    if(!strata %in% names(bbs_strata)) {
+      stop("Invalid stratification specified, choose one of '",
+           paste0(names(bbs_strata), collapse = "', '"), "'", call. = FALSE)
+    } else {
+      return(strata)
+    }
   }
-  strata
+
+  # Working with a custom stratification
+  if(!strata %in% names(bbs_strata)) {
+
+    if(is.null(custom) || !inherits(custom, "sf")) {
+      stop("Invalid stratification specified, choose one of '",
+           paste0(names(bbs_strata), collapse = "', '"),
+           "',\n or provide an sf spatial data frame to `strata_custom` ",
+           "to use a custom stratification", call. = FALSE)
+    }
+    type <- "custom"
+  }
+
+  # Working with an established stratification
+  if(strata %in% names(bbs_strata)) {
+
+    if(!is.null(custom)) {
+      # Check if strata established, and custom is a data frame
+      if(!inherits(custom, "data.frame")) {
+        stop("`strata_custom` is provided, but is not a data frame. \n",
+             "If using an establish stratification ('", strata, "'), ",
+             "`strata_custom` must be either empty, or a data frame",
+             call. = FALSE)
+      } else {
+        # Check if strata established, and custom is a subset of the correct data
+
+        # - Check cols, then check strata names
+        if(!all(names(bbs_strata[[strata]]) %in% names(custom)) ||
+           !all(custom$strata_name %in% bbs_strata[[strata]]$strata_name)) {
+          stop("`strata_custom` is not a subset of ",
+               "`bbs_strata[[\"", strata, "\"]]`.\n",
+               "If using a custom set of an established stratification ",
+               "('", strata, "'), the filtered data must have columns and ",
+               "strata names from the original", call. = FALSE)
+        }
+
+      }
+
+      # Don't modify if the exact same
+      if(!isTRUE(all.equal(bbs_strata[[strata]], custom))) {
+        type <- "subset"
+      }
+    }
+  }
+
+  if(!quiet) message("Using '", strata, "' (", type, ") stratification")
+
+  c(strata, type)
 }
 
-check_species <- function(species, species_list,
-                          name = "species (`species_to_run`)") {
-  if(is.null(species)) stop("No ", name, "specified", call. = FALSE)
+check_regions <- function(regions, stratify_by, stratify_type, regions_index = NULL) {
 
-  if(!species %in% species_list$english) {
-    stop("Invalid species specified. Ensure ", name, " matches a value in\n",
-         "the `english` column of the stratified species list ",
-         "(`strat_data$species_strat)`.",
+  r <- c("continent", "stratum", "prov_state", "bcr", "bcr_by_country")
+
+  if(!is.null(regions_index)) {
+    if(!"strata_name" %in% names(regions_index)) {
+      stop("`regions_index` must have a `strata_name` column", call. = FALSE)
+    }
+
+    r <- union(r, names(regions_index))
+    r <- r[r != "strata_name"]
+  }
+
+  if(!all(regions %in% r)) {
+    stop("`regions` must be any of `", paste0(r, collapse = "`, `"), "`",
          call. = FALSE)
   }
-  species
+
+  msg <- "Stratification does not match desired regions:\n"
+
+  if(stratify_by %in% c("bcr", "latlong") &
+     any(regions %in% c("country", "prov_state"))) {
+    stop(msg,
+         "BCRs and lat-long degree block stratifications can not be divided ",
+         "into regions with political boundaries ('country', 'prov_state').",
+         call. = FALSE)
+  }
+
+  if(stratify_by == "prov_state" & "bcr" %in% regions){
+    stop(msg,
+         "The States and Provinces stratification",
+         "can not be divided into BCR regions.",
+         call. = FALSE)
+  }
+
+  # Check custom
+  if(stratify_type == "custom" && is.null(regions_index) &&
+     any(!regions %in% c("continent", "stratum"))) {
+      stop(msg, "Custom stratifications can only be divided into 'stratum' ",
+           "and 'continent' regions, unless a `region_index` is provided.",
+           call. = FALSE)
+  }
 }
 
-check_neighbours <- function(spatial_data, strata) {
+
+#' Get AOU numerical code of a species
+#'
+#' Return the 4-5 digit BBS code for the given species by matching English,
+#' French, or Scientific names, or AOU codes.
+#'
+#' @param species Character. English name of species
+#' @param species_list Data frame. Species List
+#' @param combine_species_forms Logical. Whether to use combined or not
+#'
+#' @return 4-5 digit AOU code for the given species
+#'
+#' @noRd
+check_species <- function(species, species_list, combine_species_forms,
+                          quiet = FALSE) {
+  nm <- deparse(substitute(species))
+  if(missing(species) || is.null(species)) {
+    stop("No `", nm, "` specified", call. = FALSE)
+  }
+
+  species <- tolower(species)
+  s_list_combo <- dplyr::filter(species_list, .data$unid_combined == TRUE)
+  s_list_no_combo <- dplyr::filter(species_list, .data$unid_combined == FALSE)
+
+  s_combo <- dplyr::filter(
+    s_list_combo,
+    .env$species == tolower(.data$english) |
+      .env$species == tolower(.data$french) |
+      .env$species == tolower(paste(.data$genus, .data$species)) |
+      .env$species == .data$aou)
+
+  s_no_combo <- dplyr::filter(
+    s_list_no_combo,
+    .env$species == tolower(.data$english) |
+      .env$species == tolower(.data$french) |
+      .env$species == tolower(paste(.data$genus, .data$species)) |
+      .env$species == .data$aou)
+
+
+  if(nrow(s_combo) == 0 & nrow(s_no_combo) == 0) {
+    stop("Invalid species. Ensure `", nm, "` matches a ",
+         "English or French species name or AOU code.\n",
+         "See `search_species()` for a generic search.",
+         call. = FALSE)
+  } else if(nrow(s_no_combo) == 0 & !combine_species_forms & nrow(s_combo) == 1){
+    stop("`combine_species_forms = FALSE` but '",
+         s_combo$english, "' is a combined form...\nif you want this species, set ",
+         "`combine_species_forms = TRUE`", call. = FALSE)
+  } else if(nrow(s_combo) == 0 & combine_species_forms & nrow(s_no_combo) == 1){
+    stop("`combine_species_forms = TRUE` but '",
+         s_no_combo$english, "' is an unidentified,\nnon-combined form...",
+         "if you want this species, set ",
+         "`combine_species_forms = FALSE`", call. = FALSE)
+  } else if((combine_species_forms & nrow(s_combo) > 1) ||
+            (!combine_species_forms & nrow(s_no_combo) > 1)) {
+    stop("Multiple species matched. ",
+         "See `search_species()`for a generic search.", call. = FALSE)
+  }
+  if(combine_species_forms) s <- s_combo else s <- s_no_combo
+
+  if(!quiet) message("Filtering to species ", s$english, " (", s$aou, ")")
+  s$aou
+}
+
+check_spatial <- function(spatial_data, strata) {
 
   # Check for correct data
-  if(!is.list(spatial_data) ||
-     !all(c("n", "n_edges", "node1", "node2", "adj_matrix", "strata_name") %in%
-          names(spatial_data))) {
-    stop("`spatial_data` must a list created by `prepare_spatial()` ",
-         "containing\n  at least `n`, `n_edges`, `node1`, `node2`, `adj_matrix` ",
-         "and `strata_names`", call. = FALSE)
-  }
+  check_data(spatial_data)
 
   # Check for matching strata
   if(!all(spatial_data$strata_names %in% strata)) {
@@ -101,7 +277,6 @@ check_neighbours <- function(spatial_data, strata) {
   }
 }
 
-
 #' Check user supplied sf object
 #'
 #' Quietly passes `NULL` objects through
@@ -109,38 +284,69 @@ check_neighbours <- function(spatial_data, strata) {
 #' @param sf sf spatial data frame
 #'
 #' @noRd
-check_sf <- function(sf) {
+check_sf <- function(sf, check_poly = FALSE, col = FALSE) {
   if(!is.null(sf)) {
+    nm <- deparse(substitute(sf))
+
+    # Check type and not empty
     if(!inherits(sf, "sf")) {
-      stop("`'`", deparse(substitute(sf)), "`'` must be an 'sf' spatial data frame",
-           call. = FALSE)
+      stop("`", nm, "` must be an 'sf' spatial data frame", call. = FALSE)
     } else if(nrow(sf) == 0) {
-      stop("Empty spatial data frame (`", deparse(substitute(sf)), "`)",
-           call. = FALSE)
+      stop("Empty spatial data frame (`", nm, "`)", call. = FALSE)
+    }
+
+    # Check for invalid properties
+    if(any(!sf::st_is_valid(sf))) {
+      stop("Invalid spatial properties found ",
+           "(not all `sf::st_is_valid(", nm, ")` are TRUE\n",
+           "Consider using `sf::st_make_valid()` first.", call. = FALSE)
+    }
+
+    # Check for correct column names
+    if(col && !"strata_name" %in% names(sf)) {
+      stop("`", nm, "` missing column `strata_name`", call. = FALSE)
+    }
+
+    # Check for feature types
+    if(check_poly) {
+      geo_type <- get_geo_types(sf)
+      if(!all(geo_type == "POLYGON")) {
+        stop("Spatial data (`", nm, "`) must be comprised of ",
+             "(MULTI)POLYGONS only", call. = FALSE)
+      }
     }
   }
 }
 
+check_rnaturalearth <- function() {
+  if(!requireNamespace("rnaturalearth", quietly = TRUE)){
+    stop("The 'rnaturalearth' package is required for this function.\n",
+         "You can install with: `install.packages('rnaturalearth')`",
+         call. = FALSE)
+  }
+}
 
-check_type <- function(msg, check_fun, ...) {
+
+check_type <- function(msg, check_fun, allow_null, ...) {
   args <- list(...)
   if(is.null(names(args))) {
     names(args) <- vapply(substitute(list(...))[-1], deparse, FUN.VALUE = "a")
   }
-
   ck <- vapply(args, check_fun, FUN.VALUE = TRUE)
+  if(allow_null) ck <- ck | vapply(args, is.null, FUN.VALUE = TRUE)
   if(!all(ck)) {
     stop("`", paste0(names(ck[!ck]), collapse = "`, `"),
          "` must be ", msg, call. = FALSE)
   }
 }
 
-check_logical <- function(...) {
-  check_type(msg = "logical (TRUE/FALSE)", check_fun = is.logical, ...)
+check_logical <- function(..., allow_null = FALSE) {
+  check_type(msg = "logical (TRUE/FALSE)", check_fun = is.logical, allow_null,
+             ...)
 }
 
-check_numeric <- function(...) {
-  check_type(msg = "a number", check_fun = is.numeric, ...)
+check_numeric <- function(..., allow_null = FALSE) {
+  check_type(msg = "a number", check_fun = is.numeric, allow_null, ...)
 }
 
 check_in <- function(arg, opts) {
