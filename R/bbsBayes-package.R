5331c170f22dfd71aaf36f6cb2976c6330b3f318
steffilazerte <steffi@steffi.ca>
2022-11-02 15:54:41 -0500

    housekeeping

--- R/bbsBayes-package.R
+++ R/bbsBayes-package.R
@@ -1,22 +1,2 @@
 #' @keywords internal
 "_PACKAGE"
-
-.onLoad <- function(libname, pkgname)
-{
-  assign("maps",
-         list(bbs_usgs = "BBS_USGS_strata",
-              bbs_cws = "BBS_CWS_strata",
-              state = "BBS_ProvState_strata",
-              bcr = "BBS_BCR_strata",
-              latlong = "BBS_LatLong_strata"),
-         parent.env(environment()))
-
-  assign("strata",
-         list(bbs_usgs = "strat.csv",
-              bbs_cws = "stratcan.csv",
-              state = "stateprov.csv",
-              bcr = "bcr.csv",
-              latlong = "db.csv"),
-         parent.env(environment()))
-
-}
--- data-raw/data_examples.R
+++ data-raw/data_examples.R
@@ -1,28 +1,20 @@
-# Example data
+# Example data - NEW
 d <- load_bbs_data(level = "state", release = 2020)
 
-birds_sample <- dplyr::filter(d$birds, aou == 7221)
-routes_sample <- dplyr::semi_join(d$routes, birds_sample,
-                                  by = c("country_num", "state_num", "route",
-                                         "route_data_id", "rpid", "year", "bcr"))
-species_sample <- dplyr::filter(d$species, aou == 7221)
+bbs_data_sample <- list(
+  birds = dplyr::filter(d$birds, aou == 7221),
+  routes = dplyr::semi_join(d$routes, birds_sample,
+                            by = c("country_num", "state_num", "route",
+                                   "route_data_id", "rpid", "year", "bcr")),
+  species = dplyr::filter(d$species, aou == 7221))
 
-usethis::use_data(birds_sample, routes_sample, species_sample,
-                  overwrite = TRUE)
+# Example data - ORIG
+bbs_data_sample <- list(
+  birds = dplyr::rename_with(bird_sample, snakecase::to_snake_case) %>%
+    dplyr::rename(country_num = countrynum, state_num = statenum),
+  routes = dplyr::rename_with(route_sample, snakecase::to_snake_case) %>%
+    dplyr::rename(country_num = countrynum, state_num = statenum),
+  species = dplyr::rename_with(species_sample, snakecase::to_snake_case))
 
-
-# Mini Stan models to use in examples and testing
-
-s <- stratify(by = "bbs_usgs", sample_data = TRUE)
-p <- prepare_data(s, species = "Pacific Wren", min_max_route_years = 2)
-
-pawr_fit <- run_model(p,
-                      model = "slope",
-                      out_name = "pacific_wren_slope",
-                      out_dir = system.file("extdata", package = "bbsBayes"),
-                      iter_sampling = 10, iter_warmup = 10)
-
-
-readr::write_rds(pawr_fit,
-                 file = file.path(system.file("extdata", package = "bbsBayes"),
-                                  "pacific_wren_slope_fit.rds"))
+# Choose which to keep (for now ORIG useful for ensuring nothing has changed)
+usethis::use_data(bbs_data_sample, overwrite = TRUE)
