a6fe0205fda0b2dfe95d3f18a181f09abb8fc311
steffilazerte <steffi@steffi.ca>
2022-12-13 16:48:34 -0600

    Use new sample data

--- data-raw/data_examples.R
+++ data-raw/data_examples.R
@@ -1,20 +1,20 @@
 # Example data - NEW -----------------------------------------------------
-# d <- load_bbs_data(level = "state", release = 2020)
-#
-# bbs_data_sample <- list(
-#   birds = dplyr::filter(d$birds, aou == 7221),
-#   routes = dplyr::semi_join(d$routes, birds_sample,
-#                             by = c("country_num", "state_num", "route",
-#                                    "route_data_id", "rpid", "year", "bcr")),
-#   species = dplyr::filter(d$species, aou == 7221))
+d <- load_bbs_data(level = "state", release = 2020)
+
+bbs_data_sample <- list(
+  birds = dplyr::filter(d$birds, aou == 7221),
+  routes = dplyr::semi_join(d$routes, dplyr::filter(d$birds, aou == 7221),
+                            by = c("country_num", "state_num", "route",
+                                   "route_data_id", "rpid", "year", "bcr")),
+  species = dplyr::filter(d$species, aou == 7221))
 
 # Example data - ORIG
-bbs_data_sample <- list(
-  birds = dplyr::rename_with(bird_sample, snakecase::to_snake_case) %>%
-    dplyr::rename(country_num = countrynum, state_num = statenum),
-  routes = dplyr::rename_with(route_sample, snakecase::to_snake_case) %>%
-    dplyr::rename(country_num = countrynum, state_num = statenum),
-  species = dplyr::rename_with(species_sample, snakecase::to_snake_case))
+# bbs_data_sample <- list(
+#   birds = dplyr::rename_with(bird_sample, snakecase::to_snake_case) %>%
+#     dplyr::rename(country_num = countrynum, state_num = statenum),
+#   routes = dplyr::rename_with(route_sample, snakecase::to_snake_case) %>%
+#     dplyr::rename(country_num = countrynum, state_num = statenum),
+#   species = dplyr::rename_with(species_sample, snakecase::to_snake_case))
 
 
 # Choose which to keep (for now ORIG useful for ensuring nothing has changed)
