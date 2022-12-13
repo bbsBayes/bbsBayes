ad1cede23924b3ee08643fd5bf96f07832639b32
steffilazerte <steffi@steffi.ca>
2022-12-13 17:13:07 -0600

    Add utils and helper tests

---
+++ tests/testthat/test-00_helpers.R
@@ -0,0 +1,67 @@
+test_that("search_species()", {
+
+  d <- data.frame(
+    term = c("Paridae", "chickadee", "mésang", "Poecile"),
+    match = c("(c|C)hickadee|Titmouse", "(c|C)hickadee",
+              "(c|C)hickadee|Jay|(t|T)it", "(c|C)hickadee"))
+
+  for(i in seq_len(nrow(d))) {
+    expect_silent(s <- search_species(d$term[i])) %>%
+      expect_s3_class("data.frame")
+    expect_gt(nrow(s), 0)
+    expect_named(s, names(bbs_data_sample$species)[-1])
+    expect_true(all(stringr::str_detect(s$english, d$match[i])))
+  }
+
+  # Combinable
+  expect_silent(s <- search_species("blue grouse"))
+  expect_equal(nrow(s), 1)
+
+  expect_silent(s <- search_species("sooty grouse"))
+  expect_equal(nrow(s), 2)
+
+  # To combine or not
+  expect_silent(s <- search_species("blue grouse",
+                                    combine_species_forms = FALSE))
+  expect_equal(nrow(s), 0)
+
+  expect_silent(s <- search_species("northern flicker \\(all forms\\)"))
+  expect_equal(nrow(s), 1)
+  expect_silent(s <- search_species("northern flicker \\(all",
+                                    combine_species_forms = FALSE))
+  expect_equal(nrow(s), 0)
+})
+
+test_that("load_map()", {
+  for(b in names(bbs_strata)) {
+    expect_silent(m <- load_map(b))
+    expect_s3_class(m, "sf")
+  }
+})
+
+
+test_that("assign_prov_state()", {
+
+  map <- load_map("prov_state")
+
+  expect_silent(d <- assign_prov_state(map))
+  expect_s3_class(d, "sf")
+
+  expect_silent(d <- assign_prov_state(map, plot = TRUE))
+  expect_s3_class(d, "sf")
+
+  expect_silent(d <- assign_prov_state(map, keep_spatial = FALSE))
+  expect_s3_class(d, "data.frame")
+  expect_false(inherits(d, "sf"))
+
+
+  map <- load_map("bcr")
+  expect_error(d <- assign_prov_state(map, min_overlap = 0.4))
+  expect_warning(d <- assign_prov_state(map, min_overlap = 0.51))
+  expect_s3_class(d, "sf")
+
+  map <- load_map("bbs_usgs")
+  expect_warning(d <- assign_prov_state(map))
+  expect_silent(d <- assign_prov_state(map, min_overlap = 0.51))
+})
+
--- tests/testthat/test-00_utils.R
+++ tests/testthat/test-00_utils.R
@@ -1,13 +1,29 @@
-test_that("load_maps()", {
-
-  load_map("bbs_cws")
-
-})
-
-
 test_that("ext()", {
   expect_equal(ext("hello"), NA_character_)
   expect_equal(ext("hello.txt"), "txt")
   expect_equal(ext("hello.tiff"), "tiff")
 })
 
+test_that("load_internal_file()", {
+  for(b in c("bbs_cws", "bbs_usgs")) {
+    expect_silent(f <- load_internal_file("geofacet-grids", b))
+    expect_s3_class(f, "data.frame")
+  }
+})
+
+test_that("get_geo_types()", {
+  m <- load_map("bbs_cws")
+  expect_silent(get_geo_types(m)) %>%
+    expect_equal("POLYGON")
+
+  pts <- load_bbs_data(sample = TRUE)$routes %>%
+    sf::st_as_sf(coords = c("longitude", "latitude"))
+  expect_silent(get_geo_types(pts)) %>%
+    expect_equal("POINT")
+})
+
+
+test_that("format_ne_states()", {
+  expect_silent(ps <- format_ne_states()) %>%
+    expect_s3_class("sf")
+})
